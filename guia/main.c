798608095695f4472bcd715d010647703b024e55
Marcos Rios <marcosarios@gmail.com>
2020-07-14 23:13:29 -0300

    remove exams from repository, add cmakelist config and fix main.c

--- guia/main.c
+++ guia/main.c
@@ -11,45 +11,39 @@ int _isAlpha(const char c);
 
 int main(int cargs, char **varg, char **vent) {
     // Seteamos locale para poder utilizar acentos directamente en printf
-//    setlocale(LC_ALL, "");
-//    puts("=========================================");
-//    puts("  Ejercicios Programación 1 - Guia 2020  ");
-//    puts("=========================================");
-//
-//    int tp = 0;
-//    int ej = 0;
-//    do {
-//        printf("Indica el TP a ejecutar (0 para salir): ");
-//        fflush(stdin);
-//        scanf("%d", &tp);
-//        if(tp != 0) {
-//            if(tp != 1 && tp != 2 && tp != 3) {
-//                printf("\nEl número de TP no existe!\n\n");
-//            } else {
-//                printf("\n>>>>> TP%d\n", tp);
-//                printf("> Indica el ejercicio a ejecutar: ");
-//                scanf("%d", &ej);
-//                fflush(stdin);
-//                switch(tp) {
-//                case 1:
-//                    correr_ejercicio_tp1(ej);
-//                    break;
-//                case 2:
-//                    correr_ejercicio_tp2(ej);
-//                    break;
-//                default:
-//                    printf("El número de TP no existe.");
-//                }
-//            }
-//            printf("\n**************************************************\n");
-//        }
-//    } while(tp != 0);
-    char linea1[] = "     mientras palpite mi fiel corazon.                 \n";
-    char linea2[] = "     jurando amarte, como asi defenderte,              \n";
-    int res = comparar_lineas(linea1, linea2);
-    int res2 = comparar_lineas(linea2, linea1);
-    printf("Resultado: %d", res);
-    printf("Resultado2: %d", res2);
+    setlocale(LC_ALL, "");
+    puts("=========================================");
+    puts("  Ejercicios Programaciï¿½n 1 - Guia 2020  ");
+    puts("=========================================");
+
+    int tp = 0;
+    int ej = 0;
+    do {
+        printf("Indica el TP a ejecutar (0 para salir): ");
+        fflush(stdin);
+        scanf("%d", &tp);
+        if(tp != 0) {
+            if(tp != 1 && tp != 2 && tp != 3) {
+                printf("\nEl nï¿½mero de TP no existe!\n\n");
+            } else {
+                printf("\n>>>>> TP%d\n", tp);
+                printf("> Indica el ejercicio a ejecutar: ");
+                scanf("%d", &ej);
+                fflush(stdin);
+                switch(tp) {
+                case 1:
+                    correr_ejercicio_tp1(ej);
+                    break;
+                case 2:
+                    correr_ejercicio_tp2(ej);
+                    break;
+                default:
+                    printf("El nï¿½mero de TP no existe.");
+                }
+            }
+            printf("\n**************************************************\n");
+        }
+    } while(tp != 0);
     return 0;
 }
 
--- parciales/ea3-cpp/Figura.cpp
+++
@@ -1,3 +0,0 @@
-/**//* 35.588.676-RIOS,MarcosAdrian-(01-2964) *//**/
-
-#include "Figura.h"
