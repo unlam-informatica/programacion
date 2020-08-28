5caf7f070ca9f5fd00adea3f1a7a05386b943dfb
Marcos <marcosarios@gmail.com>
2020-08-28 21:23:01 +0100

    implementacion lista simple

--- tda/lista/lista_simple.h
+++ tda/lista/lista_simple.h
@@ -1,14 +1,33 @@
 #ifndef LISTA_SIMPLE_H_INCLUDED
 #define LISTA_SIMPLE_H_INCLUDED
 
+#define minimo(X, Y) ((X) <= (Y) ? (X) : (Y))
+
 typedef struct sNodo {
     void *info;
     unsigned tamInfo;
     struct sNodo *sig;
 } Nodo;
 
-typedef Nodo* Lista;
+typedef Nodo* ListaSimple;
 
-void crearLista(Lista *p);
+
+void crearListaSimple(ListaSimple *p);
+
+int listaSimpleVacia(const ListaSimple *p);
+
+void vaciarLista(ListaSimple *p);
+
+int listaSimpleLlena(const ListaSimple *p, unsigned tamElem);
+
+int ponerAlInicioListaSimple(ListaSimple *p, const void *d, unsigned tamElem);
+
+int ponerAlFinalListaSimple(ListaSimple *p, const void *d, unsigned tamElem);
+
+int verPrimeroListaSimple(const ListaSimple *p, void *d, unsigned tamElem);
+
+int sacarPrimeroListaLlena(ListaSimple *p, void *d, unsigned tamElem);
+
+int verUltimoListaSimple(const ListaSimple *p, void *d, unsigned tamElem);
 
 #endif // LISTA_SIMPLE_H_INCLUDED
